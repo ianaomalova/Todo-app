c5a3f25af305bddf21eea6055150b63b8f0adb67
ianaomalova <69366481+ianaomalova@users.noreply.github.com>
2025-01-07 21:00:51 +0300

    added completed task

--- src/components/Todos/ToDo/Todo.jsx
+++ src/components/Todos/ToDo/Todo.jsx
@@ -1,18 +1,19 @@
-import { IoCheckmarkDoneSharp } from "react-icons/io5";
-import { MdOutlineDelete } from "react-icons/md";
-import { GrTask } from "react-icons/gr";
+import {IoCheckmarkDoneSharp} from "react-icons/io5";
+import {MdOutlineDelete} from "react-icons/md";
+import {GrTask} from "react-icons/gr";
 import cn from 'classnames';
 import style from './Todo.module.css'
 
 const Todo = ({todo, deleteTodo, toggleTodo}) => {
   return (
-    <div className={cn(style.toDoElement)} onDoubleClick={() => deleteTodo(todo.id)}>
+    <div className={cn(style.toDoElement, {[style.completed]: todo.completed})}
+         onDoubleClick={() => toggleTodo(todo.id)}>
       <div className={style.leftInfo}>
         <GrTask className={style.iconTask}/>
         <p className={style.titleTask}>{todo.text}</p>
       </div>
       <div className={style.rightInfo}>
-        <IoCheckmarkDoneSharp className={style.icon} onClick={() => toggleTodo(todo.id)} />
+        <IoCheckmarkDoneSharp className={style.icon} onClick={() => toggleTodo(todo.id)}/>
         <MdOutlineDelete className={style.icon} onClick={() => deleteTodo(todo.id)}/>
       </div>
     </div>
