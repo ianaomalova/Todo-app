501afc341c7e5bba639f4c296ce060d25c7fc056
ianaomalova <69366481+ianaomalova@users.noreply.github.com>
2025-01-07 17:53:31 +0300

    add icon for task

--- src/components/Todos/ToDo/Todo.jsx
+++ src/components/Todos/ToDo/Todo.jsx
@@ -3,7 +3,7 @@ import style from './Todo.module.css'
 const Todo = () => {
   return (
     <div className={style.toDoElement}>
-      <img src='' alt=''/>
+      <img src='/src/assets/icons/check.png' alt=''/>
       <h2 className={style.titleTask}>Title task</h2>
     </div>
   )
